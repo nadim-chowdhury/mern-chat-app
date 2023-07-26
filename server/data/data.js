f646d57ec3e7e13a8133591b4f216f477b836cb5
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-07-26 12:36:11 +0600

    react installed

--- server/data/data.js
+++ server/data/data.js
@@ -1,5 +1,7 @@
 const chats = [
   {
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "John Doe",
     isGroupChat: false,
     users: [
       {
@@ -11,10 +13,10 @@ const chats = [
         email: "nadim@mail.com",
       },
     ],
-    _id: "617a077e18c25468bc7c4dd4",
-    chatName: "John Doe",
   },
   {
+    _id: "617a077e18c25468b27c4dd4",
+    chatName: "Guest User",
     isGroupChat: false,
     users: [
       {
@@ -26,10 +28,10 @@ const chats = [
         email: "nadim@mail.com",
       },
     ],
-    _id: "617a077e18c25468b27c4dd4",
-    chatName: "Guest User",
   },
   {
+    _id: "617a077e18c2d468bc7c4dd4",
+    chatName: "Anthony",
     isGroupChat: false,
     users: [
       {
@@ -41,10 +43,10 @@ const chats = [
         email: "nadim@mail.com",
       },
     ],
-    _id: "617a077e18c2d468bc7c4dd4",
-    chatName: "Anthony",
   },
   {
+    _id: "617a518c4081150716472c78",
+    chatName: "Friends",
     isGroupChat: true,
     users: [
       {
@@ -60,14 +62,14 @@ const chats = [
         email: "guest@example.com",
       },
     ],
-    _id: "617a518c4081150716472c78",
-    chatName: "Friends",
     groupAdmin: {
       name: "Guest User",
       email: "guest@example.com",
     },
   },
   {
+    _id: "617a077e18c25468bc7cfdd4",
+    chatName: "Jane Doe",
     isGroupChat: false,
     users: [
       {
@@ -79,10 +81,10 @@ const chats = [
         email: "nadim@mail.com",
       },
     ],
-    _id: "617a077e18c25468bc7cfdd4",
-    chatName: "Jane Doe",
   },
   {
+    _id: "617a518c4081150016472c78",
+    chatName: "Chill Zone",
     isGroupChat: true,
     users: [
       {
@@ -98,11 +100,11 @@ const chats = [
         email: "guest@example.com",
       },
     ],
-    _id: "617a518c4081150016472c78",
-    chatName: "Chill Zone",
     groupAdmin: {
       name: "Guest User",
       email: "guest@example.com",
     },
   },
 ];
+
+module.exports = { chats };
--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
 
-app.listen(8000, () => {
-  console.log("Server Started at 8000");
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Started at ${PORT}`);
 });
