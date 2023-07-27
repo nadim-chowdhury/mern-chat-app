606e67b3326bfefcfaa2694fa2163e165c12ea66
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-07-27 15:18:53 +0600

    user controller implemented

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,13 +13,7 @@ app.use(cors());
 dotenv.config();
 connectDB();
 
-app.get("/", (req, res) => {
-  res.json({ message: "ok" });
-});
-
-app.get("/api/chats", (req, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
 
 app.get("/api/chats/:id", (req, res) => {
   const singleChat = chats.find((c) => c._id === req.params.id);
