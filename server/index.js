53d56d061028a8a5a91e4968ec1e5cf97ab35551
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-07-29 16:55:06 +0600

    middleware implemented in backend

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const notfound = require("./middlewares/notFound");
+const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +17,8 @@ dotenv.config();
 connectDB();
 
 app.use("/api/user", userRoutes);
+app.use(notfound);
+app.use(errorHandler);
 
 app.get("/api/chats/:id", (req, res) => {
   const singleChat = chats.find((c) => c._id === req.params.id);
